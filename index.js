eadc287dc045e50c8be024091a7ea34b08cd788b
Yan Hong <onlyhongyan@gmail.com>
2017-05-20 11:31:20 -0700

    add socket io integration

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
  */
 var express = require('express')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 var path = require('path')
 var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
@@ -54,6 +56,20 @@ app.post('/student/add', function(req, res) {
     })
 })
 
-app.listen(3000, function () {
+app.get('/chat', function(req, res) {
+    res.sendFile(__dirname + '/public/chat.html')
+})
+
+io.on('connection', function(socket) {
+    console.log('a user joined!')
+    socket.on('send msg', function(msg) {
+        io.emit('show msg', msg)
+    })
+    socket.on('disconnect', function () {
+        console.log('user disconnected.')
+    })
+})
+
+http.listen(3000, function () {
     console.log('Haha Hehe!')
 })
